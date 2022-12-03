2c926fed5f862603cc360f79cce309128a0daf2b
filipjt88 <filip88ks@gmail.com>
2022-12-03 19:14:02 +0100

    save

---
+++ main.js
@@ -0,0 +1,21 @@
+let verticalNavbar = document.querySelector(".vertical-navbar");
+let nav                   = document.querySelector("nav");
+let bars                 = document.querySelector(".fa-bars");
+
+verticalNavbar.addEventListener("click", () => {
+    if(nav.style.width === "" && nav.style.height === "") {
+        nav.style.width = "500px";
+        nav.style.height = "100%";
+        nav.style.background = "#000";
+        nav.style.transition = "width 3s, height 3s";
+        bars.style.color = "#fff";
+        bars.style.transform = "rotate(90deg)";
+        bars.style.transition = "1s";
+    } else if(nav.style.width === "500px" && nav.style.height === "100%") {
+        nav.style.width =  "";
+        nav.style.height = "";
+        nav.style.background = "#000";
+        bars.style.transform = "rotate(0deg)";
+        bars.style.transition = "1s";
+    }
+});
